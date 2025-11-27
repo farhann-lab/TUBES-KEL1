677c9115a8467f2ae2eb0fe70b6ec4d541cf64cf
farhann-lab <mfarhanharahap22@gmail.com>
2025-11-27 14:32:18 +0700

    pesan commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void headerMenu(){
+    int pilihan;
+    printf("+=======================================+\n");
+    printf("|              ATM MACHINE              |\n");
+    printf("+=======================================+\n");
+    printf("|1. Tarik Uang\t\t\t\t|\n");
+    printf("|2. Setor Uang\t\t\t\t|\n");
+    printf("|3. Transfer Uang\t\t\t|\n");
+    printf("|4. Lihat Saldo\t\t\t\t|\n");
+    printf("|5. Riwayat Transaksi\t\t\t|\n");
+    printf("|6. Lucky Draw\t\t\t\t|\n");
+    printf("|7. Keluar\t\t\t\t|\n");
+    printf("+=======================================+\n");
+    printf("Pilih menu: ");
+    // scanf("%d", &pilihan);
+
+}
+int main(){
+    headerMenu();
+    return 0;
+
+}
