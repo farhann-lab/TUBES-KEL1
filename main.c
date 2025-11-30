bc8af8a695242f16633a9511c73dce391fc92552
farhann-lab <mfarhanharahap22@gmail.com>
2025-11-30 19:33:31 +0700

    update akun

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
+#include "akun.h"
 
-struct Akun
-{
-    char nama[50];
-    char pin[20];
-    long saldo;
-};
- 
-void headerMenu(){
-    int pilihan;
+void headerMenu(struct Account *user){
+    int pilihan, loop;
+
+    while (1) {
     printf("+=======================================+\n");
     printf("|              ATM MACHINE              |\n");
     printf("+=======================================+\n");
@@ -18,18 +14,57 @@ void headerMenu(){
     printf("|4. Lihat Saldo\t\t\t\t|\n");
     printf("|5. Riwayat Transaksi\t\t\t|\n");
     printf("|6. Lucky Draw\t\t\t\t|\n");
-    printf("|7. Keluar\t\t\t\t|\n");
+    printf("|7. Ganti Pin\t\t\t\t|\n");
+    printf("|8. Keluar\t\t\t\t|\n");
     printf("+=======================================+\n");
     printf("Pilih menu: ");
-    // scanf("%d", &pilihan);
+    scanf("%d", &pilihan);
 
-}
+    switch (pilihan)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+
+        break;
+    case 4:
+        printf("Saldo Anda: %ld\n", user->saldo);
+        break;
+    case 5:
+        
+        break;
+    case 6:
+        
+        break;
+    case 7:
+        ubahPin(user);
+        break;
+    case 8:
+        printf("Keluar dari menu.\n");
+        return;
+        break;
+    default:
+        break;
+    }
+    }
+    }
 int main(){
-    struct Akun akun;
 
+    struct Account user;
 
-    
-    headerMenu();
+    if (!akunAda()) {
+        buatAkun(&user);
+    } else {
+        ambilAkun(&user);
+    }
+
+    login(&user);
+
+    headerMenu(&user);
     return 0;
 
 }
