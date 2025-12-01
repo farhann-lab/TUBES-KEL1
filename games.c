ce9b9f91ba2332f465eb5917e086bbfea0ba7d86
lildevilzz <sugehartodanielle@gmail.com>
2025-12-01 19:58:44 +0700

    biar rapi

--- games.c
+++ games.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include "transaksi.h" 
-//test
+
+
 void games(struct Account *acc) {
     long saldo = acc -> saldo;
     int gamble, roll;
@@ -10,10 +11,16 @@ void games(struct Account *acc) {
     int cocok = 0;
     long hadiah = 0;
 
+
+
+
     if (saldo < 20000) {
         printf("You are too broke to gamble, minimal 20 ribu lah\n");
         return;
     }
+
+
+
 
     printf("Gamble 3 digits (000-999): ");
     if (scanf("%d", &gamble) != 1) {
@@ -26,11 +33,17 @@ void games(struct Account *acc) {
         return;
     }
 
+
+
+
     // fee
     acc->saldo -= 20000;
 
+
     srand((unsigned)time(NULL));
     roll = rand() % 1000;
+
+
 
     g1 = gamble / 100;
     g2 = (gamble / 10) % 10;
@@ -54,8 +67,12 @@ void games(struct Account *acc) {
     simpanSaldo(acc);
     simpanRiwayat("LUCKY", hadiah, acc->saldo);
 
+
+    
     time_t now = time(NULL);
     struct tm *w = localtime(&now);
+
+
 
     printf("[%02d-%02d-%04d %02d:%02d] Roll: %03d\n | Gamble: %03d\n | Cocok: %d\n | Hadiah: %ld\n | Fee: -20000\n",
            w->tm_mday, w->tm_mon + 1, w->tm_year + 1900,
